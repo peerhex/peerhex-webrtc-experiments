fbe81c5461d66d7393de6d988817512fa71e3aff
Jim Pick <jim@protocol.ai>
2020-04-05 20:17:52 -0700

    Add react, example webrtc-star code

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
-const WStar = require('libp2p-webrtc-star')
-const multiaddr = require('multiaddr')
-const pipe = require('it-pipe')
-const { collect } = require('streaming-iterables')
+import { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+import { html } from 'htm/react'
+import Libp2p from 'libp2p'
+import WebRTCStar from 'libp2p-webrtc-star'
+import Secio from 'libp2p-secio'
+import Mplex from 'libp2p-mplex'
+import produce from 'immer'
 
-console.log('Jim1')
+function App (props) {
+  const [logs] = useState([])
+  const [_, forceUpdate] = useState(0)
 
-const addr = multiaddr('/ip4/127.0.0.1/tcp/9090/ws/p2p-webrtc-star/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSooo2a')
+  function log (txt) {
+    logs.push(txt)
+    forceUpdate(Date.now())
+  }
 
-const ws = new WStar({ upgrader })
+  useEffect(() => {
+    async function run () {
+      const libp2p = await Libp2p.create({
+        modules: {
+          transport: [WebRTCStar],
+          connEncryption: [Secio],
+          streamMuxer: [Mplex]
+        }
+      })
 
-/*
-const listener = ws.createListener((socket) => {
-  console.log('new connection opened')
-  pipe(
-    ['hello'],
-    socket
-  )
-})
+      const webrtcAddr = '/ip4/0.0.0.0/tcp/9090/wss/p2p-webrtc-star'
+      libp2p.peerInfo.multiaddrs.add(webrtcAddr)
 
-await listener.listen(addr)
-console.log('listening')
+      libp2p.on('peer:discovery', peerInfo => {
+        log(`Found peer ${peerInfo.id.toB58String()}`)
+      })
 
-const socket = await ws.dial(addr)
-const values = await pipe(
-  socket,
-  collect
+      // Listen for new connections to peers
+      libp2p.on('peer:connect', peerInfo => {
+        log(`Connected to ${peerInfo.id.toB58String()}`)
+      })
+
+      // Listen for peers disconnecting
+      libp2p.on('peer:disconnect', peerInfo => {
+        log(`Disconnected from ${peerInfo.id.toB58String()}`)
+      })
+
+      await libp2p.start()
+    }
+    run()
+  }, [])
+  return html`
+    <div>
+      <h3>Logs</h3>
+      ${logs.map((line, i) => {
+        return html`<div key=${i}>${line}</div>`
+      })}
+    </div>
+  `
+}
+
+const appEl = document.getElementById('app')
+ReactDOM.createRoot(appEl).render(
+  html`
+    <${App} />
+  `
 )
-
-console.log(`Value: ${values.toString()}`)
-
-// Close connection after reading
-await listener.close()
-*/
